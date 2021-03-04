eded62ff033223c637018b6337fc339196a2a2a1
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2021-03-04 08:24:02 -0700

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MainBodyComponent } from './main-body/main-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MainBodyComponent
   ],
   imports: [
     BrowserModule,
